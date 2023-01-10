22c0c77750d7a61be7d5c2e8b2e53f89a2f089cb
BuildTools <scr33py294@gmail.com>
2023-01-10 19:37:36 +0700

    frontend update

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
-const listTabsElem = document.querySelector(".action--list-tabs");
-const tabsBarElem = document.querySelector(".tab-bar");
-const urlBarElem = document.querySelector(".URL-bar");
-const urlParams = new URLSearchParams(location.search);
-const currentPage = urlParams.get("page");
+// const listTabsElem = document.querySelector(".action--list-tabs");
+// const tabsBarElem = document.querySelector(".tab-bar");
+// const urlBarElem = document.querySelector(".URL-bar");
+// const urlParams = new URLSearchParams(location.search);
+// const currentPage = urlParams.get("page");
 
-listTabsElem.addEventListener("click", () => {
-    tabsBarElem.classList.toggle("tabs-listed");
-    listTabsElem.classList.toggle("tabs-listed");
-});
+// listTabsElem.addEventListener("click", () => {
+//     tabsBarElem.classList.toggle("tabs-listed");
+//     listTabsElem.classList.toggle("tabs-listed");
+// });
 
-urlBarElem.innerText = `Page ${currentPage}`;
+// urlBarElem.innerText = `Page ${currentPage}`;
