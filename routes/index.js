a21d52e47944d8b26d09b0c570bc4408dbf18262
BuildTools <scr33py294@gmail.com>
2023-01-11 21:29:34 +0700

    for loop solos

--- routes/index.js
+++ routes/index.js
@@ -15,35 +15,35 @@ try {
    client.on("ready", async () => {
       console.log(`${client.user.username} is ready`);
       client.user.setPresence({
-         activities: [
-            {
-               name: "#owners-gallery",
-               type: "WATCHING"
-            },
-         ],
+         activities: [{
+            name: "#owners-gallery",
+            type: "WATCHING"
+         }],
          status: "idle",
-      }); 
+      });
    });
 
    const removeDuplicates = async () => {
       const docs = await upload.find({});
       const dedupedDocs = docs.filter((doc, index, self) => self.findIndex(d => d.id === doc.id) === index);
       const duplicates = docs.filter((doc) => !dedupedDocs.includes(doc));
-      duplicates.forEach(async (duplicate) => {
-         await upload.deleteOne({ _id: duplicate._id });
-      });
+      for (let i = 0; i < duplicates.length; i++) {
+         await upload.deleteOne({
+            _id: duplicates[i]._id
+         });
+      }
    };
 
    const processAttachments = async (messages) => {
       let id = 0;
       const attachments = [];
-      messages.forEach((message) => {
-         const attach = Array.from(message.attachments.values()).map(({url}) => url);
-         const desc = message.content;
-         const date = timeConverter(message.createdTimestamp);
-         const username = message.author.username;
-         const discriminator = message.author.discriminator;
-         const reactions = Array.from(message.reactions.cache.values()).map(({ _emoji }) => ({
+      for (let i = 0; i < messages.length; i++) {
+         const attach = Array.from(messages[i].attachments.values()).map(({url}) => url);
+         const desc = messages[i].content;
+         const date = timeConverter(messages[i].createdTimestamp);
+         const username = messages[i].author.username;
+         const discriminator = messages[i].author.discriminator;
+         const reactions = Array.from(messages[i].reactions.cache.values()).map(({_emoji}) => ({
             name: _emoji.name,
             id: _emoji.id || null,
             count: _emoji.reaction.count
@@ -57,8 +57,7 @@ try {
             attach,
             reactions
          });
-      });
-      attachments.reverse();
+      }
       for (let i = 1; i < attachments.length; i++) {
          if (attachments[i].date === attachments[i - 1].date) {
             attachments[i - 1].desc += `\n\n${attachments[i].desc}`;
@@ -76,51 +75,50 @@ try {
             i--;
          }
       }
-      return attachments;
+      return attachments.reverse();
    };
-   
+
    const updateAttachments = async (guildId, channelId) => {
       await removeDuplicates();
-      upload.find({}, async function (err, docs) {
-         const guild = client.guilds.cache.get(guildId);
-         const channel = await guild.channels.fetch(channelId);
-         const messages = await channel.messages.fetch();
-         const attachments = await processAttachments(messages);
-         const old = JSON.parse(fs.readFileSync("./models/attachments.json"));
-         const hasNewProperty = attachments.some(a => {
-            for (const prop in a) {
-               if (!old.some(o => prop in o)) {
-                  return true;
-               }
+      const docs = await upload.find({});
+      const guild = client.guilds.cache.get(guildId);
+      const channel = await guild.channels.fetch(channelId);
+      const messages = await channel.messages.fetch();
+      const attachments = await processAttachments(messages);
+      const old = JSON.parse(fs.readFileSync("./models/attachments.json"));
+      const hasNewProperty = attachments.some(a => {
+         for (const prop in a) {
+            if (!old.some(o => prop in o)) {
+               return true;
             }
-            return false;
-         });
-         if (hasNewProperty) {
-            await upload.deleteMany({});
          }
-         const newAttachments = Array.from(new Set(attachments.filter(a => !old.some(o => o.id === a.id))));
-         const newAttach = Array.from(new Set(old.concat(newAttachments)));
-         const filtered = Array.from(new Set(newAttach.filter(a => !docs.some(o => o.id === a.id))));
-         if (filtered.length > 0) {
-            filtered.forEach(async (a) => {
-               await upload.create({
-                  id: a.id,
-                  desc: a.desc,
-                  date: a.date,
-                  username: a.username,
-                  discriminator: a.discriminator,
-                  attach: a.attach,
-                  reactions: a.reactions,
-               });
-            });
-         } else {
-            return;
-         }
-         fs.writeFileSync("./models/attachments.json", JSON.stringify(newAttach));
-         await removeDuplicates();
+         return false;
       });
+      if (hasNewProperty) {
+         await upload.deleteMany({});
+      }
+      const newAttachments = Array.from(new Set(attachments.filter(a => !old.some(o => o.id === a.id))));
+      const newAttach = Array.from(new Set(old.concat(newAttachments)));
+      const filtered = Array.from(new Set(newAttach.filter(a => !docs.some(o => o.id === a.id))));
+      if (filtered.length > 0) {
+         for (let i = 0; i < filtered.length; i++) {
+            await upload.create({
+               id: filtered[i].id,
+               desc: filtered[i].desc,
+               date: filtered[i].date,
+               username: filtered[i].username,
+               discriminator: filtered[i].discriminator,
+               attach: filtered[i].attach,
+               reactions: filtered[i].reactions,
+            });
+         }
+      } else {
+         return;
+      }
+      fs.writeFileSync("./models/attachments.json", JSON.stringify(newAttach));
+      await removeDuplicates();
    };
-   
+
    exports.fetch = async (guildId, channelId) => {
       await updateAttachments(guildId, channelId);
    };
