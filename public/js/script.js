89716e48a96d8010708cb56cafa7772a411b6c15
Zarcka <99590517+Zarcka@users.noreply.github.com>
2023-01-11 20:36:30 +0400

    Fixed issues #5 and #6

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,7 @@
-// const listTabsElem = document.querySelector(".action--list-tabs");
-// const tabsBarElem = document.querySelector(".tab-bar");
-// const urlBarElem = document.querySelector(".URL-bar");
-// const urlParams = new URLSearchParams(location.search);
-// const currentPage = urlParams.get("page");
+const listTabsElem = document.querySelector(".action--list-tabs");
+const tabsBarElem = document.querySelector(".tab-bar");
 
-// listTabsElem.addEventListener("click", () => {
-//     tabsBarElem.classList.toggle("tabs-listed");
-//     listTabsElem.classList.toggle("tabs-listed");
-// });
-
-// urlBarElem.innerText = `Page ${currentPage}`;
+listTabsElem.addEventListener("click", () => {
+    tabsBarElem.classList.toggle("tabs-listed");
+    listTabsElem.classList.toggle("tabs-listed");
+});
